36e458991b818aa8929029226c1d4c72dd26b279
GitHub Action <action@github.com>
2025-08-01 06:17:16 +0000

    chore: Auto-format code with Black and Prettier [GitHub Action]

--- scripts/config.js
+++ scripts/config.js
@@ -18,11 +18,9 @@ const QUIZ_CONFIG = {
   style: "legacy", // Can be "default", "legacy", or "dark"
 };
 
-
 // Global boolean variable to indicate if the quiz is running in standalone mode
 window.notStandalone = true;
 /*
 Need to use this always in a conditional as window.notStandalone, it is important for re to find
 it and delete the block for the python code to work correctly.
 */
-
